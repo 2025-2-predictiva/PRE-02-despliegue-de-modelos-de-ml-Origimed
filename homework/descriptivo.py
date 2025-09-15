f95c1ce182df0cc0e2aa57c4e14ac4c3a5f497e1
origimed <allopezp@unal.edu.co>
2025-09-14 19:36:55 -0500

    completado2

---
+++ homework/descriptivo.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("/home/ptro/Code/un/semestre 6/predictiva/PRE-02-despliegue-de-modelos-de-ml-Origimed/files/input/house_data.csv", sep=",")
+
+print(df.head())
